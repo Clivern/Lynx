dee7e8d2d2c068c370c482ec94ac0d3a1a632bff
Clivern <hello@clivern.com>
2023-02-07 01:31:34 +0100

    updates

--- config/config.exs
+++ config/config.exs
@@ -34,7 +34,7 @@ config :esbuild,
   version: "0.14.29",
   default: [
     args:
-      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+      ~w(--bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
