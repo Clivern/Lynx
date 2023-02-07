9b44b4a874920f913fa302bbb304469f2fe07ac8
Clivern <hello@clivern.com>
2023-02-07 02:46:45 +0100

    updates

--- lib/bandit_web/endpoint.ex
+++ lib/bandit_web/endpoint.ex
@@ -24,7 +24,7 @@ defmodule BanditWeb.Endpoint do
     at: "/",
     from: :bandit,
     gzip: false,
-    only: ~w(assets fonts images favicon.ico robots.txt)
+    only: ~w(assets theme fonts images favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
