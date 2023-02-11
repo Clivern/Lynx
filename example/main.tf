215e63b101552a0c5dd1ba76b9f57977ba216de9
Clivern <hello@clivern.com>
2023-02-12 00:12:59 +0100

    feat: update env vars

--- example/main.tf
+++ example/main.tf
@@ -7,11 +7,11 @@ terraform {
   }
 
   backend "http" {
-    username       = "vf41bkwj"
-    password       = "2sTb&N*gvyXj"
-    address        = "http://64.225.70.88:4000/client/temper/monitoring/prod/state"
-    lock_address   = "http://64.225.70.88:4000/client/temper/monitoring/prod/lock"
-    unlock_address = "http://64.225.70.88:4000/client/temper/monitoring/prod/unlock"
+    # username     = ""
+    # password     = ""
+    address        = "http://64.225.70.88/client/temper/monitoring/prod/state"
+    lock_address   = "http://64.225.70.88/client/temper/monitoring/prod/lock"
+    unlock_address = "http://64.225.70.88/client/temper/monitoring/prod/unlock"
     lock_method    = "POST"
     unlock_method  = "POST"
   }
@@ -19,21 +19,18 @@ terraform {
   required_version = "1.3.7"
 }
 
-provider "local" {
-}
+provider "local" {}
 
 resource "local_file" "hello_world" {
   content  = "Hello, World!"
   filename = "${path.module}/hello.txt"
 }
 
-provider "time" {
-}
+provider "time" {}
 
 resource "null_resource" "previous" {}
 
 resource "time_sleep" "wait_30_seconds" {
-  depends_on = [null_resource.previous]
-
+  depends_on      = [null_resource.previous]
   create_duration = "30s"
 }
