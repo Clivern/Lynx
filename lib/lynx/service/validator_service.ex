e77a1e53a7fc9c5375b104ef9498572f0c0c5701
Clivern <hello@clivern.com>
2023-03-20 22:35:11 +0100

    fixes

--- lib/lynx/service/validator_service.ex
+++ lib/lynx/service/validator_service.ex
@@ -12,43 +12,6 @@ defmodule Lynx.Service.ValidatorService do
   alias Lynx.Context.EnvironmentContext
   alias Lynx.Exception.InvalidRequest
 
-  @link_regex Regex.compile!(
-    "^" <>
-      # protocol identifier
-      "(?:(?:https?|ftp)://)" <>
-      # user:pass authentication
-      "(?:\\S+(?::\\S*)?@)?" <>
-      "(?:" <>
-        # IP address exclusion
-        # private & local networks
-        "(?!(?:10|127)(?:\\.\\d{1,3}){3})" <>
-        "(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})" <>
-        "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" <>
-        # IP address dotted notation octets
-        # excludes loopback network 0.0.0.0
-        # excludes reserved space >= 224.0.0.0
-        # excludes network & broacast addresses
-        # (first & last IP address of each class)
-        "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])" <>
-        "(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}" <>
-        "(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))" <>
-      "|" <>
-        # host name
-        "(?:(?:[a-z\\x{00a1}-\\x{ffff}0-9]-*)*[a-z\\x{00a1}-\\x{ffff}0-9]+)" <>
-        # domain name
-        "(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}0-9]-*)*[a-z\\x{00a1}-\\x{ffff}0-9]+)*" <>
-        # TLD identifier
-        "(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}]{2,}))" <>
-        # TLD may end with dot
-        "\\.?" <>
-      ")" <>
-      # port number
-      "(?::\\d{2,5})?" <>
-      # resource path
-      "(?:[/?#]\\S*)?" <>
-    "$", "iu"
-  )
-
   def is_number?(value, err) do
     case is_number(value) do
       true -> {:ok, value}
@@ -121,9 +84,10 @@ defmodule Lynx.Service.ValidatorService do
   end
 
   def is_url?(value, err) do
-    case Regex.match?(@link_regex, value) do
-      true -> {:ok, value}
-      false -> {:error, err}
+    case URI.parse(url) do
+      %URI{scheme: nil} -> {:error, err}
+      %URI{host: nil} -> {:error, err}
+      _ -> {:ok, value}
     end
   end
 
