4d29454afd4bf964c19ae560ea15f9c61482db1e
ngdangdat <ngdangdat09@gmail.com>
2026-04-29 18:48:32 +0900

    fix: drop Plug.Parsers init assertion that required json_decoder

--- test/lynx_web/endpoint_test.exs
+++ test/lynx_web/endpoint_test.exs
@@ -9,12 +9,5 @@ defmodule LynxWeb.EndpointTest do
     test "defaults to 8 MB so existing deployments are unaffected" do
       assert Application.get_env(:lynx, LynxWeb.Endpoint)[:http_max_body_length] == 8_000_000
     end
-
-    test "is wired into Plug.Parsers as the :length option" do
-      configured = Application.get_env(:lynx, LynxWeb.Endpoint)[:http_max_body_length]
-      parsers_opts = Plug.Parsers.init(parsers: [:urlencoded, :json], length: configured)
-
-      assert parsers_opts.length == configured
-    end
   end
 end
